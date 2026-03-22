31de163e3e7b7aed0fe39af4538c171d7fac5829
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2026-03-22 23:12:59 +0800

    vTbmassoc is now vtNet, a tbmassoc network!!🌙

--- app/_global/_global.tsx
+++ app/_global/_global.tsx
@@ -49,7 +49,7 @@ export const Footer: React.FC = () => {
           </div>
           <br />
           <p style={{ fontSize: "0.625em" }}>
-            © 2022 a vTbmassoc project&ensp;
+            ©2022 vtNet, a tbmassoc network&ensp;
             <i
               style={{ display: "inline-block" }}
               className="bi bi-moon-fill"
